df3395d3dc35d9944ae1f2b62296284af7cedf9c
Dnyaneshwar_K <eluminous_sedk@eluminoustechnologies.com>
2020-11-07 16:41:57 +0530

    first commit

---
+++ src/core/components/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <section className="content-section bg-light" id="about">
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col-lg-10 mx-auto">
+                        <h2>My Portfolio Website!</h2>
+                        <p className="lead mb-5">This is just a website to present my work with you!</p>
+                        <a className="btn btn-dark btn-xl js-scroll-trigger" href="#services">What I Offer</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Profile
---
+++ src/core/pages/Home.js
@@ -0,0 +1,20 @@
+import react from 'react'
+import Navbar from '../components/Navbar'
+import Profile from '../components/Profile'
+import Portfolio from '../components/Portfolio'
+import Services from '../components/Services'
+import Footer from '../components/Footer'
+
+const Home = () => {
+    return (
+        <div id="page-top">
+            <Navbar />
+            <Profile />
+            <Services />
+            <Portfolio />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
